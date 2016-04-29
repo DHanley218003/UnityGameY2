fa417bbd6424016e14b89df95343d82ddb4109e2
DHanley218003 <bassmaster965@yahoo.com>
2016-04-29 02:50:53 +0100

    20/4/16

--- Assets/Code/GUI/scoreSheet.js
+++ Assets/Code/GUI/scoreSheet.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 var health:int = 100;
 var score:int = 0;
+var win:boolean = false;
 var style:GUIStyle;
 
 function Start()
@@ -13,6 +14,16 @@ function OnGUI()
 {
 	GUI.Label(Rect(10,10,100,20), "Score: " + score.ToString(), style);
 	GUI.Label(Rect(10,20,100,20), "Health: " + health.ToString(), style);
+	if(win)
+	{
+		GUI.Label(Rect(10,10,100,20), "You Win!", style);
+		Time.timeScale = 0;
+	}
+	else if(health <= 0)
+	{
+		GUI.Label(Rect(10,10,100,20), "You Lost...", style);
+		Time.timeScale = 0;
+	}
 }
 
 function hit()
@@ -32,4 +43,8 @@ function heal()
 function kill()
 {
 	score += 25;
+}
+function winGame()
+{
+	win = true;
 }
---
+++ Assets/Code/Player/Win.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var style:GUIStyle;
+
+function Start () 
+{
+	style.name = "HUD";
+	style.normal.textColor = Color.black;
+}
+
+function OnCollisionEnter2D(col:Collision2D)
+{
+	if(col.gameObject == "Guy")
+	{
+		GameObject.Find("Main Camera").GetComponent(scoreSheet).winGame();
+	}
+}
